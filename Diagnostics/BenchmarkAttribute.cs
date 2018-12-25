ba778c53f5eb8628fce493c63aff1d66e39ab4c3
Cristian Moraru <heisenberg@thekrystalship.org>
2018-12-25 17:26:53 +0200

    Added Benchmark project.

---
+++ Diagnostics/BenchmarkAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TheKrystalShip.Tools.Diagnostics
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class BenchmarkAttribute : Attribute, IDisposable
+    {
+        private readonly Stopwatch _stopwatch;
+
+        public BenchmarkAttribute()
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            Console.WriteLine($"Elapsed time: {_stopwatch.Elapsed.ToString()}");
+        }
+    }
+}
