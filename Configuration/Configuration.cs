5fda21357c895e3cf5c75e113c3a9e3dbf964a94
Cristian Moraru <heisenberg@thekrystalship.org>
2018-12-29 20:17:27 +0200

    Added Configuration project.

---
+++ Configuration/Configuration.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace TheKrystalShip.Tools.Configuration
+{
+    public static class Configuration
+    {
+        private static string _basePath;
+        private static IConfiguration _config;
+
+        static Configuration()
+        {
+            _basePath = Directory.GetCurrentDirectory();
+        }
+
+        /// <summary>
+        /// Set the base path to load configuration files from
+        /// </summary>
+        /// <param name="path">Existing directory to be used as base path for configuration files</param>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        public static void SetBasePath(string path)
+        {
+            if (!Directory.Exists(path))
+                throw new DirectoryNotFoundException(nameof(path));
+
+            _basePath = path;
+        }
+
+        /// <summary>
+        /// Add configuration files
+        /// </summary>
+        /// <param name="files">Collection of configuration files to add</param>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static void AddFiles(params string[] files)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(_basePath);
+
+            foreach (string file in files)
+            {
+                if (!File.Exists(file))
+                    throw new FileNotFoundException("File not found", file);
+
+                configurationBuilder.AddJsonFile(path: file, optional: true, reloadOnChange: true);
+            }
+
+            _config = configurationBuilder.Build();
+        }
+
+        /// <summary>
+        /// Gets a configuration value
+        /// </summary>
+        /// <param name="key">The configuration key</param>
+        /// <returns>The configuration value</returns>
+        public static string Get(string key)
+        {
+            return _config[key];
+        }
+
+        /// <summary>
+        /// Set a configuration value
+        /// </summary>
+        /// <param name="key">The configuration key</param>
+        /// <param name="newValue">The configuration value</param>
+        public static void Set(string key, string newValue)
+        {
+            _config[key] = newValue;
+        }
+
+        /// <summary>
+        /// Shorthand for GetSection("ConnectionStrings")[name]
+        /// </summary>
+        /// <param name="key">The configuration key</param>
+        /// <returns>The configuration value</returns>
+        public static string GetConnectionString(string key)
+        {
+            return _config.GetConnectionString(key);
+        }
+
+        /// <summary>
+        /// Gets a configuration sub-section with the specified key
+        /// </summary>
+        /// <param name="key">The key of the configuration section.</param>
+        /// <returns>The Microsoft.Extensions.Configuration.IConfigurationSection</returns>
+        public static IConfigurationSection GetSection(string key)
+        {
+            return _config.GetSection(key);
+        }
+
+        /// <summary>
+        /// Gets the immediate descendant configuration sub-sections.
+        /// </summary>
+        /// <returns>The configuration sub-sections.</returns>
+        public static IEnumerable<IConfigurationSection> GetChildren()
+        {
+            return _config.GetChildren();
+        }
+
+        /// <summary>
+        /// Returns a Microsoft.Extensions.Primitives.IChangeToken that can be used to observe
+        /// when this configuration is reloaded.
+        /// </summary>
+        /// <returns>A Microsoft.Extensions.Primitives.IChangeToken.</returns>
+        public static IChangeToken GetReloadToken()
+        {
+            return _config.GetReloadToken();
+        }
+    }
+}
---
+++ Configuration/README.md
@@ -0,0 +1,48 @@
+# TheKrystalShip.Tools.Configuration
+
+This library is a static wrapper around `Microsoft.Extensions.IConfiguration`, exposing exisitng methods thourhg a static `Configuration` class.
+
+# Usage
+
+There's a couple methods in the `Configuration` class that need to be called at startup.
+
+## 1. Setting the base path
+
+```cs
+Configuration.SetBasePath(Path.Combine("path", "to", "config", "files"));
+```
+
+## 2. Adding configuration files
+
+`Configuration` only works with `.json` files.
+
+> All files are added as optional, and with `reloadOnChange` active by default.
+
+```cs
+Configuration.AddFiles("file1.json", "file2.json", ...);
+```
+
+## Read value
+
+```cs
+string myValue = Configuration.Get("someKey");
+```
+
+## Set value
+
+```cs
+Configuration.Set("someKey", "myNewValue")
+```
+
+Here's a list of all methods that `Configuration` offers:
+
+```cs
+void SetBasePath(string path);
+void AddFiles(params string[] files);
+string Get(string key);
+void Set(string key, string newValue);
+string GetConnectionString(string key);
+IConfigurationSection GetSection(string key);
+IEnumerable<IConfigurationSection> GetChildren();
+IChangeToken GetReloadToken();
+```
